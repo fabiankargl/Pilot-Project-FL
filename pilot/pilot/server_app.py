a2e7fb667b7973407a3ebce43fd890fa3afc8554
Fabian Kargl <fabian.kargl@outlook.com>
2025-12-09 15:12:42 +0100

    update server_app.py and toml to fit fiven problem

--- pilot/pilot/server_app.py
+++ pilot/pilot/server_app.py
@@ -1,31 +1,36 @@
-"""pilot: A Flower / PyTorch app."""
-
 import torch
 from flwr.app import ArrayRecord, ConfigRecord, Context
 from flwr.serverapp import Grid, ServerApp
 from flwr.serverapp.strategy import FedAvg
 
-from pilot.task import Net
+from pilot.task import BankNet, load_data
 
-# Create ServerApp
 app = ServerApp()
-
 
 @app.main()
 def main(grid: Grid, context: Context) -> None:
-    """Main entry point for the ServerApp."""
-
     # Read run config
     fraction_train: float = context.run_config["fraction-train"]
+    fraction_evaluate: float = context.run_config["fraction-evaluate"]
     num_rounds: int = context.run_config["num-server-rounds"]
     lr: float = context.run_config["lr"]
+    min_clients: int = context.run_config["min-available-clients"]
+    
+    trainloader, _ = load_data(partition_id=0,
+                               num_partitions=0)
+    sample_batch = next(iter(trainloader))
+    input_dim = sample_batch[0].shape[1]
 
     # Load global model
-    global_model = Net()
+    global_model = BankNet(input_dim=input_dim)
     arrays = ArrayRecord(global_model.state_dict())
 
     # Initialize FedAvg strategy
-    strategy = FedAvg(fraction_train=fraction_train)
+    strategy = FedAvg(fraction_train=fraction_train,
+                      fraction_evaluate=fraction_evaluate,
+                      min_fit_clients=min_clients,
+                      min_evaluate_clients=min_clients,
+                      min_available_clients=min_clients)
 
     # Start strategy, run FedAvg for `num_rounds`
     result = strategy.start(
